edc64d6b8b97bfa40dbb4511291b422bbab4b7be
Daniel Wertheim <daniel@wertheim.se>
2011-08-27 08:41:23 +0200

    [CI]: Changed convention for NUnit report.

--- Deploy/rake-build.rb
+++ Deploy/rake-build.rb
@@ -14,7 +14,7 @@ require 'albacore'
 @env_buildconfigname = ENV['env_buildconfigname'].empty? ? "Release" : ENV['env_buildconfigname']
 @env_buildfolderpath = ENV['env_buildfolderpath']
 #--------------------------------------
-#optional if no nuget actions should be performed
+#optional if no remote nuget actions should be performed
 @env_nugetPublishApiKey = ENV['env_nugetPublishApiKey']
 @env_nugetPublishUrl = ENV['env_nugetPublishUrl']
 @env_nugetSourceUrl = ENV['env_nugetSourceUrl']
@@ -23,7 +23,7 @@ require 'albacore'
 @env_solutionfolderpath = "../Solution/"
 @env_solutionname = 'Ensure.That'
 @env_projectname = 'EnsureThat'
-@env_buildversion = "0.2.0" + (@env_buildnumber.empty? ? "" : ".#{@env_buildnumber.to_s}")
+@env_buildversion = "0.2.0" + (@env_buildnumber.empty? ? "" : ".#{@env_buildnumber}")
 @env_buildname = "#{@env_solutionname}-v#{@env_buildversion}-#{@env_buildconfigname}"
 @env_buildfolderpath = "#{@env_buildfolderpath}builds/#{@env_buildname}/"
 @env_binariesfolderpath = "#{@env_buildfolderpath}binaries"
@@ -77,7 +77,7 @@ end
 
 nunit :runUnitTests do |nunit|
   nunit.command = "#{@env_solutionfolderpath}packages/NUnit.2.5.10.11092/tools/nunit-console.exe"
-  nunit.options "/framework=v4.0.30319","/xml=#{@env_buildfolderpath}NUnit-#{@env_projectname}-UnitTests.xml"
+  nunit.options "/framework=v4.0.30319","/xml=#{@env_buildfolderpath}NUnit-Report-#{@env_projectname}-UnitTests.xml"
   nunit.assemblies = FileList["#{@env_solutionfolderpath}Tests/**/#{@env_buildconfigname}/*.UnitTests.dll"].exclude(/obj\//)
 end
 
