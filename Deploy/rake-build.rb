cee11562025c85ab3a66ff7a4b8b00c3fb4802f9
Daniel Wertheim <daniel@wertheim.se>
2012-06-22 09:57:21 +0200

    Bumped to v0.8.0

--- Deploy/rake-build.rb
+++ Deploy/rake-build.rb
@@ -15,7 +15,7 @@ require 'albacore'
 @env_projectnameEnsureThat = 'EnsureThat'
 
 @env_buildfolderpath = 'build'
-@env_version = "0.7.0"
+@env_version = "0.8.0"
 @env_buildversion = @env_version + (ENV['env_buildnumber'].to_s.empty? ? "" : ".#{ENV['env_buildnumber'].to_s}")
 @env_buildconfigname = ENV['env_buildconfigname'].to_s.empty? ? "Release" : ENV['env_buildconfigname'].to_s
 @env_buildname = "#{@env_solutionname}-v#{@env_buildversion}-#{@env_buildconfigname}"
