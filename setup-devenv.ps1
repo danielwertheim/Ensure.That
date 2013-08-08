7b9c874ab66972b30f483bf6c21265cfd39b0c63
Daniel Wertheim <daniel@wertheim.se>
2013-08-08 21:56:04 +0200

    Tweak

--- setup-devenv.ps1
+++ setup-devenv.ps1
@@ -4,19 +4,19 @@ Function OutPut ($v) {
     Write-Host $v -ForegroundColor Cyan
 }
 
-Function Get-Package-Configs {
-    return Get-ChildItem "src" -Filter "packages.config" -Recurse -File
+Function Get-Package-Configs($basePath) {
+    return Get-ChildItem $basePath -Filter "packages.config" -Recurse
 }
 
-Function Install-Packages {
+Function Install-Packages($basePath) {
     OutPut "Installing missing NuGet packages..."
     $c = 0
-    Get-Package-Configs | ForEach-Object { 
+    Get-Package-Configs($basePath) | ForEach-Object {
         OutPut "Installing packages for: '$($_.FullName)'"
-        nget i ""$($_.FullName)"" -o "src\packages"
+        nget i ""$($_.FullName)"" -o "$($basePath)\packages"
         $c += 1
     }
     OutPut "Processed $($c) package(s).config."
 }
 
-Install-Packages
+Install-Packages("src")
