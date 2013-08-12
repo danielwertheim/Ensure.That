48159c2b88678a1e915993fb2f2674a1731bed9c
Daniel Wertheim <daniel@wertheim.se>
2013-08-12 23:18:51 +0200

    Simplify

--- setup-devenv.ps1
+++ setup-devenv.ps1
@@ -1,22 +1,17 @@
-Set-Alias nget nuget.exe
-
 Function OutPut ($v) {
-    Write-Host $v -ForegroundColor Cyan
-}
-
-Function Get-Package-Configs($basePath) {
-    return Get-ChildItem $basePath -Filter "packages.config" -Recurse
+  Write-Host $v -ForegroundColor Cyan
 }
 
 Function Install-Packages($basePath) {
-    OutPut "Installing missing NuGet packages..."
-    $c = 0
-    Get-Package-Configs($basePath) | ForEach-Object {
-        OutPut "Installing packages for: '$($_.FullName)'"
-        nget i ""$($_.FullName)"" -o "$($basePath)\packages"
-        $c += 1
+  OutPut "Installing missing NuGet packages..."
+  $c = 0
+  Get-ChildItem $basePath -Filter "packages.config" -Recurse |
+    ForEach-Object {
+      OutPut "Installing packages for: '$($_.FullName)'"
+      nuget i ""$($_.FullName)"" -o "$($basePath)\packages"
+      $c += 1
     }
-    OutPut "Processed $($c) package(s).config."
+  OutPut "Processed $($c) package(s).config."
 }
 
 Install-Packages("src")
