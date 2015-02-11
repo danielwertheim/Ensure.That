1e4cd8a44c1f0e419e69c2aa337b41a705fa8d7b
Daniel Wertheim <daniel@wertheim.se>
2015-02-11 22:30:49 +0100

    POC for issue #7

--- src/Projects/EnsureThat/EnsureObjectExtensions.cs
+++ src/Projects/EnsureThat/EnsureObjectExtensions.cs
@@ -6,8 +6,11 @@ namespace EnsureThat
     public static class EnsureObjectExtensions
     {
         [DebuggerStepThrough]
-        public static Param<T> IsNotNull<T>(this Param<T> param) where T : class
+        public static Param<T> IsNotNull<T>(this Param<T> param, Throws<T>.ExceptionFnConfig exceptionFn = null) where T : class
         {
+            if (exceptionFn != null)
+                throw exceptionFn(Throws<T>.Instance)(param);
+
             if (param.Value == null)
                 throw ExceptionFactory.CreateForParamNullValidation(param, ExceptionMessages.EnsureExtensions_IsNotNull);
 
