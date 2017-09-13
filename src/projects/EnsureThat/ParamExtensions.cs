23c6b44808b4ff79fcf5ec9e43c350651526e559
David <davidfallah1@gmail.com>
2017-09-13 12:23:31 +0100

    Add overload for WithExtraMessageOf to accept strings

--- src/projects/EnsureThat/ParamExtensions.cs
+++ src/projects/EnsureThat/ParamExtensions.cs
@@ -9,6 +9,13 @@ namespace EnsureThat
             return param;
         }
 
+        public static Param<T> WithExtraMessageOf<T>(this Param<T> param, string message)
+        {
+            param.ExtraMessageFn = p => message;
+
+            return param;
+        }
+
         public static Param<T> WithExtraMessageOf<T>(this Param<T> param, Func<string> messageFn)
         {
             param.ExtraMessageFn = p => messageFn();
