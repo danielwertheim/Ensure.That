5b76e0510df56386ea90820a6aec3e3ff27b335b
Daniel Wertheim <daniel@wertheim.se>
2017-09-27 20:38:47 +0200

    Mark as obsolete for future removal

--- src/projects/EnsureThat/Ensure.cs
+++ src/projects/EnsureThat/Ensure.cs
@@ -13,14 +13,17 @@ namespace EnsureThat
         public static void On() => IsActive = true;
 
         [DebuggerStepThrough]
+        [Obsolete("Use EnsureArg instead. This version will eventually be removed.", false)]
         public static Param<T> That<T>([NoEnumeration]T value, string name = Param.DefaultName) => new Param<T>(name, value);
 
         [DebuggerStepThrough]
+        [Obsolete("Use EnsureArg instead. This version will eventually be removed.", false)]
         public static Param<T> That<T>(Func<T> expression, string name = Param.DefaultName) => new Param<T>(
             name,
             expression.Invoke());
 
         [DebuggerStepThrough]
+        [Obsolete("Use EnsureArg instead. This version will eventually be removed.", false)]
         public static TypeParam ThatTypeFor<T>(T value, string name = Param.DefaultName) => new TypeParam(name, value.GetType());
     }
 }
