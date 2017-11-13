6f5848fed5e02db40f75cf77ccc84059918a8ad2
REDMOND\andrewar <ndrwrbgs@users.noreply.github.com>
2017-11-12 16:07:30 -0800

    Provide overloads that accept IComparer

--- src/projects/EnsureThat/ComparableArg.cs
+++ src/projects/EnsureThat/ComparableArg.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using EnsureThat.Extensions;
 using JetBrains.Annotations;
@@ -20,12 +21,36 @@ namespace EnsureThat
         }
 
         [DebuggerStepThrough]
+        public T Is<T>(T value, T expected, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (!value.IsEq(expected, comparer))
+                throw new ArgumentException(ExceptionMessages.Comp_Is_Failed.Inject(value, expected), paramName);
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
         public T IsNot<T>(T value, T expected, [InvokerParameterName] string paramName = Param.DefaultName) where T : IComparable<T>
         {
             if (!Ensure.IsActive)
                 return value;
 
             if (value.IsEq(expected))
+                throw new ArgumentException(ExceptionMessages.Comp_IsNot_Failed.Inject(value, expected), paramName);
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
+        public T IsNot<T>(T value, T expected, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (value.IsEq(expected, comparer))
                 throw new ArgumentException(ExceptionMessages.Comp_IsNot_Failed.Inject(value, expected), paramName);
 
             return value;
@@ -44,12 +69,36 @@ namespace EnsureThat
         }
 
         [DebuggerStepThrough]
+        public T IsLt<T>(T value, T limit, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (!value.IsLt(limit, comparer))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotLt.Inject(value, limit));
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
         public T IsLte<T>(T value, T limit, [InvokerParameterName] string paramName = Param.DefaultName) where T : IComparable<T>
         {
             if (!Ensure.IsActive)
                 return value;
 
             if (value.IsGt(limit))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotLte.Inject(value, limit));
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
+        public T IsLte<T>(T value, T limit, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (value.IsGt(limit, comparer))
                 throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotLte.Inject(value, limit));
 
             return value;
@@ -68,12 +117,36 @@ namespace EnsureThat
         }
 
         [DebuggerStepThrough]
+        public T IsGt<T>(T value, T limit, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (!value.IsGt(limit, comparer))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotGt.Inject(value, limit));
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
         public T IsGte<T>(T value, T limit, [InvokerParameterName] string paramName = Param.DefaultName) where T : IComparable<T>
         {
             if (!Ensure.IsActive)
                 return value;
 
             if (value.IsLt(limit))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotGte.Inject(value, limit));
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
+        public T IsGte<T>(T value, T limit, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (value.IsLt(limit, comparer))
                 throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotGte.Inject(value, limit));
 
             return value;
@@ -89,6 +162,21 @@ namespace EnsureThat
                 throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotInRange_ToLow.Inject(value, min));
 
             if (value.IsGt(max))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotInRange_ToHigh.Inject(value, max));
+
+            return value;
+        }
+
+        [DebuggerStepThrough]
+        public T IsInRange<T>(T value, T min, T max, IComparer<T> comparer, [InvokerParameterName] string paramName = Param.DefaultName)
+        {
+            if (!Ensure.IsActive)
+                return value;
+
+            if (value.IsLt(min, comparer))
+                throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotInRange_ToLow.Inject(value, min));
+
+            if (value.IsGt(max, comparer))
                 throw new ArgumentOutOfRangeException(paramName, value, ExceptionMessages.Comp_IsNotInRange_ToHigh.Inject(value, max));
 
             return value;
