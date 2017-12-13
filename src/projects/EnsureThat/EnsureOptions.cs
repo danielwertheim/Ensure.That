6c4a60c975d3bdbbbf6e3cead01c4ac5dc8979eb
Daniel Wertheim <daniel@wertheim.se>
2017-12-13 19:27:59 +0100

    Make state immutable

--- src/projects/EnsureThat/EnsureOptions.cs
+++ src/projects/EnsureThat/EnsureOptions.cs
@@ -7,26 +7,28 @@ namespace EnsureThat
         /// <summary>
         /// If defined, this exception will be thrown instead of the
         /// standard exceptions for the particular ensure method.
+        /// Assign using <see cref="WithException"/>.
         /// </summary>
-        public Exception CustomException;
+        public Exception CustomException { get; private set; }
 
         /// <summary>
         /// If defined, and no <see cref="CustomException"/> has been defined,
         /// this message will be used instead of the standard message for the
         /// particular ensure method.
+        /// Assign using <see cref="WithMessage"/>.
         /// </summary>
-        public string CustomMessage;
-
-        public EnsureOptions WithMessage(string message)
-        {
-            CustomMessage = message;
-
-            return this;
-        }
+        public string CustomMessage { get; private set; }
 
         public EnsureOptions WithException(Exception ex)
         {
             CustomException = ex;
+
+            return this;
+        }
+
+        public EnsureOptions WithMessage(string message)
+        {
+            CustomMessage = message;
 
             return this;
         }
