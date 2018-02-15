b8704af7d22a0fefd003ab688061fbf8f1ef381c
Daniel Wertheim <daniel@wertheim.se>
2018-02-15 21:40:28 +0100

    Bool tests

--- src/tests/UnitTests/EnsureBoolParamTests.cs
+++ src/tests/UnitTests/EnsureBoolParamTests.cs
@@ -10,22 +10,26 @@ namespace UnitTests
         public void IsTrue_WhenFalseExpression_ThrowsArgumentException() => ShouldThrow<ArgumentException>(
             ExceptionMessages.Booleans_IsTrueFailed,
             () => Ensure.Bool.IsTrue(false, ParamName),
-            () => EnsureArg.IsTrue(false, ParamName));
+            () => EnsureArg.IsTrue(false, ParamName),
+            () => Ensure.That(false, ParamName).IsTrue());
 
         [Fact]
         public void IsTrue_WhenTrueExpression_ShouldNotThrow() => ShouldNotThrow(
             () => Ensure.Bool.IsTrue(true, ParamName),
-            () => EnsureArg.IsTrue(true, ParamName));
+            () => EnsureArg.IsTrue(true, ParamName),
+            () => Ensure.That(true, ParamName).IsTrue());
 
         [Fact]
         public void IsFalse_WhenTrueExpression_ThrowsArgumentException() => ShouldThrow<ArgumentException>(
             ExceptionMessages.Booleans_IsFalseFailed,
             () => Ensure.Bool.IsFalse(true, ParamName),
-            () => EnsureArg.IsFalse(true, ParamName));
+            () => EnsureArg.IsFalse(true, ParamName),
+            () => Ensure.That(true, ParamName).IsFalse());
 
         [Fact]
         public void IsFalse_WhenFalseExpression_ShouldNotThrow() => ShouldNotThrow(
             () => Ensure.Bool.IsFalse(false, ParamName),
-            () => EnsureArg.IsFalse(false, ParamName));
+            () => EnsureArg.IsFalse(false, ParamName),
+            () => Ensure.That(false, ParamName).IsFalse());
     }
 }
