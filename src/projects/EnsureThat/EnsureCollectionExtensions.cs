fcb321d2f8cda84eaae245e8605a65fc86e41b35
Daniel Wertheim <daniel@wertheim.se>
2018-02-15 21:31:00 +0100

    To be or not to be

---
+++ src/projects/EnsureThat/EnsureCollectionExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EnsureThat
+{
+    public static class EnsureCollectionExtensions
+    {
+        public static void HasItems<T>(this Param<T> param) where T : ICollection
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<Collection<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<ICollection<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<T[]> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<List<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<IList<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<TKey, TValue>(this Param<IDictionary<TKey, TValue>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<IReadOnlyCollection<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void HasItems<T>(this Param<IReadOnlyList<T>> param)
+            => Ensure.Collection.HasItems(param.Value, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<T[]> param, int expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<T[]> param, long expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<T> param, int expected) where T : ICollection
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<T> param, long expected) where T : ICollection
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<ICollection<T>> param, int expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<ICollection<T>> param, long expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<IList<T>> param, int expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<T>(this Param<IList<T>> param, long expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<TKey, TValue>(this Param<IDictionary<TKey, TValue>> param, int expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void SizeIs<TKey, TValue>(this Param<IDictionary<TKey, TValue>> param, long expected)
+            => Ensure.Collection.SizeIs(param.Value, expected, param.Name, param.OptsFn);
+
+        public static void ContainsKey<TKey, TValue>(this Param<IDictionary<TKey, TValue>> param, TKey key)
+            => Ensure.Collection.ContainsKey(param.Value, key, param.Name, param.OptsFn);
+
+        public static void ContainsKey<TKey, TValue>(this Param<Dictionary<TKey, TValue>> param, TKey key)
+            => Ensure.Collection.ContainsKey(param.Value, key, param.Name, param.OptsFn);
+
+        public static void HasAny<T>(this Param<IList<T>> param, Func<T, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<T>(this Param<List<T>> param, Func<T, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<T>(this Param<ICollection<T>> param, Func<T, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<T>(this Param<Collection<T>> param, Func<T, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<TKey, TValue>(this Param<IDictionary<TKey, TValue>> param, Func<KeyValuePair<TKey, TValue>, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<TKey, TValue>(this Param<Dictionary<TKey, TValue>> param, Func<KeyValuePair<TKey, TValue>, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+
+        public static void HasAny<T>(this Param<T[]> param, Func<T, bool> predicate)
+            => Ensure.Collection.HasAny(param.Value, predicate, param.Name, param.OptsFn);
+    }
+}
---
+++ src/projects/EnsureThat/EnsureGuidExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EnsureThat
+{
+    public static class EnsureGuidExtensions
+    {
+        public static void IsNotEmpty(this Param<Guid> param)
+            => Ensure.Guid.IsNotEmpty(param.Value, param.Name, param.OptsFn);
+    }
+}
