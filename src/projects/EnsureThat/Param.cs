949d85a03256bcd3b19252062c209069ae0f690f
Daniel Wertheim <daniel@wertheim.se>
2018-02-17 11:31:34 +0100

    Structs it is.

--- src/projects/EnsureThat/Param.cs
+++ src/projects/EnsureThat/Param.cs
@@ -3,43 +3,6 @@ using JetBrains.Annotations;
 
 namespace EnsureThat
 {
-    //public abstract class Param
-    //{
-    //    public const string DefaultName = "";
-
-    //    public readonly string Name;
-    //    public readonly OptsFn OptsFn;
-
-    //    protected Param(string name = null, OptsFn optsFn = null)
-    //    {
-    //        Name = name ?? DefaultName;
-    //        OptsFn = optsFn;
-    //    }
-    //}
-
-    //public sealed class Param<T> : Param
-    //{
-    //    public readonly T Value;
-
-    //    public Param(string name, T value, OptsFn optsFn = null)
-    //        : base(name, optsFn)
-    //    {
-    //        Value = value;
-    //    }
-    //}
-
-    //public sealed class TypeParam : Param
-    //{
-    //    [NotNull]
-    //    public readonly Type Type;
-
-    //    public TypeParam(string name, [NotNull] Type type)
-    //        : base(name)
-    //    {
-    //        Type = type;
-    //    }
-    //}
-
     public static class Param
     {
         public const string DefaultName = "";
