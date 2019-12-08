81cc57dfbc93d1ea3434a7751c0a3257b2187715
Andrew Arbogast <ndrwrbgs@users.noreply.github.com>
2019-12-08 12:20:11 -0800

    Add 'in' modifier

--- src/projects/EnsureThat/EnsureThatTypeExtensions.cs
+++ src/projects/EnsureThat/EnsureThatTypeExtensions.cs
@@ -5,40 +5,40 @@ namespace EnsureThat
 {
     public static class EnsureThatTypeExtensions
     {
-        public static void IsInt(this TypeParam param)
+        public static void IsInt(this in TypeParam param)
             => Ensure.Type.IsInt(param.Type, param.Name, param.OptsFn);
 
-        public static void IsShort(this TypeParam param)
+        public static void IsShort(this in TypeParam param)
             => Ensure.Type.IsShort(param.Type, param.Name, param.OptsFn);
 
-        public static void IsDecimal(this TypeParam param)
+        public static void IsDecimal(this in TypeParam param)
             => Ensure.Type.IsDecimal(param.Type, param.Name, param.OptsFn);
 
-        public static void IsDouble(this TypeParam param)
+        public static void IsDouble(this in TypeParam param)
             => Ensure.Type.IsDouble(param.Type, param.Name, param.OptsFn);
 
-        public static void IsFloat(this TypeParam param)
+        public static void IsFloat(this in TypeParam param)
             => Ensure.Type.IsFloat(param.Type, param.Name, param.OptsFn);
 
-        public static void IsBool(this TypeParam param)
+        public static void IsBool(this in TypeParam param)
             => Ensure.Type.IsBool(param.Type, param.Name, param.OptsFn);
 
-        public static void IsDateTime(this TypeParam param)
+        public static void IsDateTime(this in TypeParam param)
             => Ensure.Type.IsDateTime(param.Type, param.Name, param.OptsFn);
 
-        public static void IsString(this TypeParam param)
+        public static void IsString(this in TypeParam param)
             => Ensure.Type.IsString(param.Type, param.Name, param.OptsFn);
 
-        public static void IsOfType(this TypeParam param, [NotNull] Type expectedType)
+        public static void IsOfType(this in TypeParam param, [NotNull] Type expectedType)
             => Ensure.Type.IsOfType(param.Type, expectedType, param.Name, param.OptsFn);
 
-        public static void IsNotOfType(this TypeParam param, Type expectedType)
+        public static void IsNotOfType(this in TypeParam param, Type expectedType)
             => Ensure.Type.IsNotOfType(param.Type, expectedType, param.Name, param.OptsFn);
 
-        public static void IsClass<T>(this Param<T> param)
+        public static void IsClass<T>(this in Param<T> param)
             => Ensure.Type.IsClass(param.Value, param.Name, param.OptsFn);
 
-        public static void IsClass(this TypeParam param)
+        public static void IsClass(this in TypeParam param)
             => Ensure.Type.IsClass(param.Type, param.Name, param.OptsFn);
     }
 }
--- src/projects/EnsureThat/EnsureThatValueTypeExtensions.cs
+++ src/projects/EnsureThat/EnsureThatValueTypeExtensions.cs
@@ -4,19 +4,19 @@ namespace EnsureThat
 {
     public static class EnsureThatValueTypeExtensions
     {
-        public static void IsTrue(this Param<bool> param)
+        public static void IsTrue(this in Param<bool> param)
             => Ensure.Bool.IsTrue(param.Value, param.Name, param.OptsFn);
 
-        public static void IsFalse(this Param<bool> param)
+        public static void IsFalse(this in Param<bool> param)
             => Ensure.Bool.IsFalse(param.Value, param.Name, param.OptsFn);
 
-        public static void IsNotDefault<T>(this Param<T> param) where T : struct
+        public static void IsNotDefault<T>(this in Param<T> param) where T : struct
             => Ensure.Any.IsNotDefault(param.Value, param.Name, param.OptsFn);
 
-        public static void IsNotNull<T>(this Param<T?> param) where T : struct
+        public static void IsNotNull<T>(this in Param<T?> param) where T : struct
             => Ensure.Any.IsNotNull(param.Value, param.Name, param.OptsFn);
 
-        public static void IsNotEmpty(this Param<Guid> param)
+        public static void IsNotEmpty(this in Param<Guid> param)
             => Ensure.Guid.IsNotEmpty(param.Value, param.Name, param.OptsFn);
     }
 }
