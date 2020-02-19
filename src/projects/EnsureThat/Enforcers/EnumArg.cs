aca046baebb74f58fc8734d5afd019361391b78b
Andrew Arbogast <ndrwrbgs@users.noreply.github.com>
2020-02-18 19:11:44 -0800

    Add comments

--- src/projects/EnsureThat/Enforcers/EnumArg.cs
+++ src/projects/EnsureThat/Enforcers/EnumArg.cs
@@ -5,6 +5,9 @@ namespace EnsureThat.Enforcers
 {
     public sealed class EnumArg
     {
+        /// <summary>
+        /// Confirms that the <paramref name="value"/> is defined in the enum <typeparamref name="T"/>.
+        /// </summary>
         public T IsValidEnum<T>(T value, [InvokerParameterName] string paramName = null, OptsFn optsFn = null) where T : struct, Enum
         {
             // TODO: Test performance, not sure if the BCL IsDefined implements this naively or like the fast versino
--- src/projects/EnsureThat/EnsureThatEnumExtensions.cs
+++ src/projects/EnsureThat/EnsureThatEnumExtensions.cs
@@ -2,6 +2,9 @@
 {
     public static class EnsureThatEnumExtensions
     {
+        /// <summary>
+        /// Confirms that the <paramref name="param.Value"/> is defined in the enum <typeparamref name="T"/>.
+        /// </summary>
         public static void IsValidEnum<T>(this in Param<T> param) where T : struct, System.Enum
             => Ensure.Enum.IsValidEnum(param.Value, param.Name, param.OptsFn);
     }
