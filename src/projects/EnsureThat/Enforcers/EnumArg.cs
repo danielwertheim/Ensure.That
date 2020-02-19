fec7ef215549a9c93b73a076817daa83e99159e5
Andrew Arbogast <ndrwrbgs@users.noreply.github.com>
2020-02-18 20:07:49 -0800

    Removed TODOs, moved to Issues

--- src/projects/EnsureThat/Enforcers/EnumArg.cs
+++ src/projects/EnsureThat/Enforcers/EnumArg.cs
@@ -10,10 +10,8 @@ namespace EnsureThat.Enforcers
         /// </summary>
         public T IsValidEnum<T>(T value, [InvokerParameterName] string paramName = null, OptsFn optsFn = null) where T : struct, Enum
         {
-            // TODO: Test performance, not sure if the BCL IsDefined implements this naively or like the fast versino
             if (!Enum.IsDefined(typeof(T), value))
             {
-                // TODO: I believe this 'throw' prevents inlining of this method. If that is validated to be true, we probably refactor all throws to be method calls in the 'cold-path' (error) cases
                 throw Ensure.ExceptionFactory.ArgumentOutOfRangeException(
                     string.Format(ExceptionMessages.Enum_IsValidEnum, value, typeof(T)),
                     paramName,
