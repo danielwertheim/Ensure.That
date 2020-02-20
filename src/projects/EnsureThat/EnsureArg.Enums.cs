32a2408995d1cec466c37b7b848a6e6f1f3e8bb8
Andrew Arbogast <ndrwrbgs@users.noreply.github.com>
2020-02-20 12:11:41 -0800

    Add comments about Flags enums, noticed after reviewing Enum.IsDefined implementation

--- src/projects/EnsureThat/EnsureArg.Enums.cs
+++ src/projects/EnsureThat/EnsureArg.Enums.cs
@@ -8,7 +8,23 @@ namespace EnsureThat
     {
         /// <summary>
         /// Confirms that the <paramref name="value"/> is defined in the enum <typeparamref name="T"/>.
+        /// Note that just like `Enum.IsDefined`, `Flags` based enums may be valid combination of defined values, but if the combined value
+        /// itself is not named an error will be raised. Avoid usage with `Flags` enums.
         /// </summary>
+        /// <example>
+        /// Flags example:
+        /// 
+        /// [Flags]
+        /// enum Abc{
+        ///   A = 1,
+        ///   B = 2,
+        ///   C = 4,
+        ///   AB = 3
+        /// }
+        ///
+        /// Abc.A | Abc.B IsDefined=true (due to Abc.AB)
+        /// Abc.A | Abc.C IsDefined=false (A and C are both valid, the composite is valid due to `Flags` attribute, but the composite is not a named enum value
+        /// </example>
         public static T EnumIsDefined<T>(T value, [InvokerParameterName] string paramName = null, OptsFn optsFn = null) where T : struct, Enum
             => Ensure.Enum.IsDefined(value, paramName, optsFn);
     }
