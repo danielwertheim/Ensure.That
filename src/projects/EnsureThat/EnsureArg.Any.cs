6667ce1bc25cdb24d4760d31a0cf84a0abb1ab16
Daniel Wertheim <daniel@wertheim.se>
2020-06-04 12:36:51 +0200

    Adds HasValue (#142)
    * Ignores Rider
    
    * Adds HasValue which supports null check for nullable value types and objs
    
    * No need to capture, just use the existing value
    
    * Switches implementation
    
    After measuring the boxing path was more performant (both time and alloc)
    
    * Adds more tags to package

---
+++ src/projects/EnsureThat/EnsureArg.Any.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EnsureThat.Annotations;
+using JetBrains.Annotations;
+
+namespace EnsureThat
+{
+    public static partial class EnsureArg
+    {
+        /// <summary>
+        /// Ensures value is not null.
+        /// Supports both <see cref="Nullable{T}"/> and reference types.
+        /// If you know you are dealing with a certain type, e.g struct use preferred <see cref="IsNotNull{T}(T?, string, OptsFn)"/>
+        /// overload instead as it is more performant.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="paramName"></param>
+        /// <param name="optsFn"></param>
+        /// <returns></returns>
+        /// <remarks>If you know you are dealing with e.g. a struct, the <see cref="IsNotNull{T}(T?, string, OptsFn)"/> overload is more performant.</remarks>
+        [NotNull]
+        [ContractAnnotation("value:null => halt")]
+        public static T HasValue<T>([NoEnumeration, ValidatedNotNull] T value, [InvokerParameterName] string paramName = null, OptsFn optsFn = null)
+            => Ensure.Any.HasValue(value, paramName, optsFn);
+        
+        [NotNull]
+        [ContractAnnotation("value:null => halt")]
+        public static T IsNotNull<T>([NoEnumeration, ValidatedNotNull] T value, [InvokerParameterName] string paramName = null, OptsFn optsFn = null) where T : class
+            => Ensure.Any.IsNotNull(value, paramName, optsFn);
+    }
+}
--- src/projects/EnsureThat/EnsureThatObjectExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EnsureThat
-{
-    public static class EnsureThatObjectExtensions
-    {
-        public static void IsNotNull<T>(this in Param<T> param) where T : class
-            => Ensure.Any.IsNotNull(param.Value, param.Name, param.OptsFn);
-    }
-}
