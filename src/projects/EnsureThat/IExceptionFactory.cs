02735461418655027fbee790d282f43dba335aad
Daniel Wertheim <daniel@wertheim.se>
2021-07-06 22:06:11 +0200

    Changes ExceptionFactory to Internals.DefaultExceptionFactory (#160)

--- src/projects/EnsureThat/IExceptionFactory.cs
+++ src/projects/EnsureThat/IExceptionFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using JetBrains.Annotations;
-using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
 
 namespace EnsureThat
 {
@@ -9,71 +8,5 @@ namespace EnsureThat
         Exception ArgumentException([NotNull] string defaultMessage, string paramName, OptsFn optsFn = null);
         Exception ArgumentNullException([NotNull] string defaultMessage, string paramName, OptsFn optsFn = null);
         Exception ArgumentOutOfRangeException<TValue>([NotNull] string defaultMessage, string paramName, TValue value, OptsFn optsFn = null);
-    }
-
-    public sealed class ExceptionFactory : IExceptionFactory
-    {
-        [return: NotNull]
-        [Pure]
-        public Exception ArgumentException(string defaultMessage, string paramName, OptsFn optsFn = null)
-        {
-            if (optsFn != null)
-            {
-                var opts = optsFn(new EnsureOptions());
-
-                if (opts.CustomExceptionFactory != null)
-                    return opts.CustomExceptionFactory(defaultMessage, paramName);
-
-                if (opts.CustomException != null)
-                    return opts.CustomException;
-
-                if (opts.CustomMessage != null)
-                    return new ArgumentException(opts.CustomMessage, paramName);
-            }
-
-            return new ArgumentException(defaultMessage, paramName);
-        }
-
-        [return: NotNull]
-        [Pure]
-        public Exception ArgumentNullException(string defaultMessage, string paramName, OptsFn optsFn = null)
-        {
-            if (optsFn != null)
-            {
-                var opts = optsFn(new EnsureOptions());
-
-                if (opts.CustomExceptionFactory != null)
-                    return opts.CustomExceptionFactory(defaultMessage, paramName);
-
-                if (opts.CustomException != null)
-                    return opts.CustomException;
-
-                if (opts.CustomMessage != null)
-                    return new ArgumentNullException(paramName, opts.CustomMessage);
-            }
-
-            return new ArgumentNullException(paramName, defaultMessage);
-        }
-
-        [return: NotNull]
-        [Pure]
-        public Exception ArgumentOutOfRangeException<TValue>(string defaultMessage, string paramName, TValue value, OptsFn optsFn = null)
-        {
-            if (optsFn != null)
-            {
-                var opts = optsFn(new EnsureOptions());
-
-                if (opts.CustomExceptionFactory != null)
-                    return opts.CustomExceptionFactory(defaultMessage, paramName);
-
-                if (opts.CustomException != null)
-                    return opts.CustomException;
-
-                if (opts.CustomMessage != null)
-                    return new ArgumentOutOfRangeException(paramName, value, opts.CustomMessage);
-            }
-
-            return new ArgumentOutOfRangeException(paramName, value, defaultMessage);
-        }
     }
 }
